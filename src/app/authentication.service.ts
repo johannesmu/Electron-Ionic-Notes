6c40adabb80d903a316b65ffe9b092c047342425
Johannes Muljana <johannes.muljana@ait.nsw.edu.au>
2019-01-08 15:07:24 +1100

    added signup in authentication service

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor( private afAuth:AngularFireAuth ) { 
+
+  }
+
+  //sign up method
+  signUp( email, password ){
+    this.afAuth.auth.createUserWithEmailAndPassword( email, password )
+    .catch( (error) => {
+      //check what error
+      return this.handleSignUpError(error);
+    });
+  }
+  handleSignUpError( error ){
+    let message = error.message;
+    console.log(message);
+    switch( message ){
+      case 'auth/email-already-in-use' :
+        return 'email already used';
+      case 'auth/invalid-email' :
+        return 'please use a valid email address';
+      case 'auth/operation-not-allowed' :
+        return 'sign up is not enabled at the moment';
+      case 'auth/weak-password' :
+        return 'password is weak';
+      default:
+        return null;
+    }
+  }
 }
