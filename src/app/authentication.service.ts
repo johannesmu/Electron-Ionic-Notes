2cd6a95ccba9dc283af683b2acf499b86c473d14
Johannes Muljana <johannes.muljana@ait.nsw.edu.au>
2019-01-08 12:05:10 +1100

    added auth service

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(public afAuth:AngularFireAuth ){ 
+
+  }
+  signUp(email,password){
+
+  }
+  signIn(email,password){
+
+  }
+  signIpWithGoogle(){
+
+  }
 }
